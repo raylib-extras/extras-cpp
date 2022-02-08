c17006cdcd0c191b864363c9f20ef900b3ec4e8a
Jeffery Myers <jeffm2501@gmail.com>
2022-02-08 08:05:58 -0800

    add colors

--- raylib_namespace.h
+++ raylib_namespace.h
@@ -35,4 +35,32 @@ namespace rl
 	#include "raylib.h"
 	#include "raymath.h"
 	#include "rlgl.h"
+	
+	constexpr Color  LightGray = { 200, 200, 200, 255 };
+    constexpr Color  Gray = { 128, 128, 128, 255 };
+    constexpr Color  DarkGray{ 80, 80, 80, 255 };
+    constexpr Color  Yellow{ 253, 249, 0, 255 };
+    constexpr Color  Gold{ 255, 203, 0, 255 };
+    constexpr Color  Orange{ 255, 161, 0, 255 };
+    constexpr Color  Pink{ 255, 109, 194, 255 };
+    constexpr Color  Red{ 230, 41, 55, 255 };
+    constexpr Color  Maroon{ 190, 33, 55, 255 };
+    constexpr Color  Green{ 0, 228, 48, 255 };
+    constexpr Color  Lime{ 0, 158, 47, 255 };
+    constexpr Color  DarkGreen{ 0, 117, 44, 255 };
+    constexpr Color  SkyBlue{ 102, 191, 255, 255 };
+    constexpr Color  Blue{ 0, 121, 241, 255 };
+    constexpr Color  DarkBlue{ 0, 82, 172, 255 };
+    constexpr Color  Purple{ 200, 122, 255, 255 };
+    constexpr Color  Violet{ 135, 60, 190, 255 };
+    constexpr Color  DarkPurple{ 112, 31, 126, 255 };
+    constexpr Color  Beige{ 211, 176, 131, 255 };
+    constexpr Color  Brown{ 127, 106, 79, 255 };
+    constexpr Color  DarkBrown{ 76, 63, 47, 255 };
+
+    constexpr Color  White{ 255, 255, 255, 255 };
+    constexpr Color  Black{ 0, 0, 0, 255 };
+    constexpr Color  Blank{ 0, 0, 0, 0 };
+    constexpr Color  Magenta{ 255, 0, 255, 255 };
+    constexpr Color  RayWhite{ 245, 245, 245, 255 };
 }
